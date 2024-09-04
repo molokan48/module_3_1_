595fd5f3ee32234245cc05fdeb5b7d8b9090a5dd
alexander <molokan2007@mail.ru>
2024-09-04 14:50:46 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -5,19 +5,19 @@ string = str(input('ввод строки: '))
 list_to_search_up = [s.upper() for s in list_to_search]
 
 def string_info ():
-    print('string_info' , tuple((len(string) , string.upper() , string.lower() )))
     count_calls()
+    return tuple((len(string) , string.upper() , string.lower() ))
+
 
 def is_contains ():
-    print(string.upper() in list_to_search_up)
     count_calls()
+    return string.upper() in list_to_search_up
 
 def count_calls ():
     global calls
     calls = calls + 1
 
-string_info()
-is_contains()
-
+print('string_info: ' , string_info())
+print('is_contains: ' , is_contains())
 print('calls: ' , calls)
 print('list_to_search:  ' , len(list_to_search) , ' elements')
